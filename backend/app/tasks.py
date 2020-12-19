97f43f9eca0a4d93bb15b0c02ab8800886884905
Marek <mark-now@wp.pl>
2020-12-19 18:15:05 +0100

    sth

--- backend/app/tasks.py
+++ backend/app/tasks.py
@@ -1,12 +1,13 @@
 # from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
-import app.models as mod
+# import app.models as mod
+from .models import models
 
 #end auction when date_end expire
 @shared_task(serializer='json')
 def set_inactive(auction_id):
-    auction_ob = mod.Auction.objects.get(id=auction_id)
+    auction_ob = models.Auction.objects.get(id=auction_id)
     auction_ob.is_active = False
     auction_ob.save()
     return {"status": True}
